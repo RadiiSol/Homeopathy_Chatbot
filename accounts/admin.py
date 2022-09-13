e1dd3c13149b7923de5a6895cfffd386f221c86a
Mihir <mihirpg2014@gmail.com>
2022-09-13 15:00:45 +0530

    added patient model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Patient_Medical_History, Medical_Narcotics_Sports_History
 
-# Register your models here.
+
+admin.site.register(Patient_Medical_History)
+admin.site.register(Medical_Narcotics_Sports_History)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,33 @@
+from tkinter import CASCADE
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Patient_Medical_History(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    marital_status = models.BooleanField()
+    date_of_birth = models.DateField()
+    age = models.IntegerField()
+    sex = models.CharField(max_length=1)
+    occupation = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    contact_number = models.CharField(max_length=10)
+    height = models.CharField(max_length=5)
+    weight = models.CharField(max_length=3)
+
+    def __str__(self):
+        return str(self.user)
+
+class Medical_Narcotics_Sports_History(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    medical_history_asthama = models.BooleanField(default=False)
+    medical_history_infection = models.BooleanField(default=False)
+    medical_history_tuberculosis = models.BooleanField(default=False)
+    medical_history_thyroid = models.BooleanField(default=False)
+    medical_history_hypertension = models.BooleanField(default=False)
+    medical_history_diabetes = models.BooleanField(default=False)
+    narcotics_history_smoking = models.BooleanField(default=False)
+    narcotics_history_drugs = models.BooleanField(default=False)
+    narcotics_history_alcohol = models.BooleanField(default=False)
+
+    def __str__(self):
+        return str(self.user)
